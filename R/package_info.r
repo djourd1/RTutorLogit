8ae7abd6b82d37875d40e227c9dc88ad8b0a2d8e
Damien Jourdain <damien.jourdain@cirad.fr>
2020-04-05 07:20:42 +0200

    First

---
+++ R/package_info.r
@@ -0,0 +1,9 @@
+ps.pkg.info = function() {
+  # Name of this package
+  package = "RTutorLogit"
+  
+  # Name of problem sets in the package
+  ps = c("Logit")
+  
+  list(package=package, ps = ps)
+}
